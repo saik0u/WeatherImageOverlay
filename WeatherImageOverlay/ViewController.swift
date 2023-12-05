c9cc13f4fc17108a0bc6c34e76df142d759f3732
Nikolay Dimitrov <tehnsd@gmail.com>
2023-12-05 10:08:43 +0200

    Initial Commit

---
+++ WeatherImageOverlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherImageOverlay
+//
+//  Created by Nikolay Dimitrov on 5.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
